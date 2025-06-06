bb279c9137a8b86e6568a61f729f4ec1ee37a7cb
Pavel Gorin <buteafulspace@gmail.com>
2025-06-06 14:12:39 +0300

    Добавил кнопку хайда и еще теперь адс закрывается сам он заебал

--- scripts/content.js
+++ scripts/content.js
@@ -11,10 +11,28 @@ let isInited = false;
 
 const searchFieldClassName = 'searchField'
 const searchField = document.createElement('input');
-searchField.className = searchFieldClassName;
-searchField.placeholder = 'Search...'
-searchField.type = 'search'
+    searchField.className = searchFieldClassName
+    searchField.placeholder = 'Search...'
+    searchField.type =  'search'
+const hideButton = document.createElement('button')
+hideButton.className = 'hideButton';
+hideButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+<g id="Iconly/Curved/Hide">
+<g id="Hide">
+<path id="Stroke 1" d="M6.42 17.7298C4.19 16.2698 2.75 14.0698 2.75 12.1398C2.75 8.85984 6.89 4.83984 12 4.83984C14.09 4.83984 16.03 5.50984 17.59 6.54984" stroke="#130F26" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path id="Stroke 3" d="M19.8498 8.61035C20.7408 9.74035 21.2598 10.9904 21.2598 12.1404C21.2598 15.4204 17.1098 19.4404 11.9998 19.4404C11.0898 19.4404 10.2008 19.3104 9.36975 19.0804" stroke="#130F26" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path id="Stroke 5" d="M9.76572 14.3672C9.17072 13.7782 8.83772 12.9752 8.84072 12.1382C8.83672 10.3932 10.2487 8.97518 11.9947 8.97218C12.8347 8.97018 13.6407 9.30318 14.2347 9.89718" stroke="#130F26" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path id="Stroke 7" d="M15.1095 12.6992C14.8755 13.9912 13.8645 15.0042 12.5725 15.2412" stroke="#130F26" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path id="Stroke 9" d="M19.8917 4.25L4.11768 20.024" stroke="#130F26" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+</g>
+</svg>`
+hideButton.dataset.hide = 0
+const btwContainer = document.createElement('div')
+btwContainer.className = 'btwContainer'
 
+btwContainer.append(searchField)
+btwContainer.append(hideButton)
 
 const addFavoriteButton = (transition) => {
 
@@ -30,10 +48,49 @@ const addFavoriteButton = (transition) => {
         transition.prepend(favoriteButton)
         
     }
-
 }
 
+const onHideButtonClick = (event) =>{
+    const isHidden = !!+event.currentTarget.dataset.hide;
+    event.currentTarget.dataset.hide = Number(!isHidden);
+    searchField.value = ''
 
+    const allTransitions = document.querySelectorAll(`div[data-transition]`);
+
+
+    if(isHidden){
+        allTransitions.forEach((targetTransition)=>{
+            targetTransition.style.maxHeight = '100px'; 
+            targetTransition.style.opacity = 1; 
+            targetTransition.style.overflow = 'auto'
+        });
+
+        return;
+    }
+
+    allTransitions.forEach((targetTransition)=>{
+        targetTransition.style.maxHeight = '0px'; 
+        targetTransition.style.opacity = 0; 
+        targetTransition.style.overflow = 'hidden'
+    })   
+}
+
+const closeSupportAdsModal = () =>{ 
+
+    const findAdsModal = () => {
+        const overlay = document.querySelector('.player-overlay-background--darkness-3');
+   
+        if(overlay){
+            const closeBtn = overlay.querySelector('button');
+            closeBtn.click()
+
+        }
+    }
+
+    setInterval(()=>{
+        findAdsModal()
+    }, 1000)
+}
 
 const showFavoritesList = (event) => {
     const twitchMoreButton = document.querySelector(`button[data-a-target="side-nav-show-more-button"]`);
@@ -55,7 +112,7 @@ const showFavoritesList = (event) => {
 
             if(isTargetTransition){
                 transition.style.transition = 'all .2s';
-
+                transition.dataset.transition = true;
                 addFavoriteButton(transition)
                 const streamerName = transition.querySelector('p[data-a-target="side-nav-title"]')?.textContent
     
@@ -88,6 +145,7 @@ const showFavoritesList = (event) => {
 
 
 const onSearchHandler = (event) => {
+    hideButton.dataset.hide = '0'
     let hiddenTransitionsLength = 0;
     const regex = new RegExp(event.target.value, 'gi');
 
@@ -161,15 +219,19 @@ const sideBarResizeObserver = new ResizeObserver((entries) => {
     document.addEventListener('click', onFavoriteClickHandler)
     
     transitionHTMLList = Array.from(target.children).filter((transition) => {
+    
         transition.style.transition = 'all .2s';
         addFavoriteButton(transition)
         return transition.className.includes(transitionClassName)
     })
     
-    target.prepend(searchField)
+    target.prepend(btwContainer)
     searchField.addEventListener('input', onSearchHandler)
     searchField.addEventListener('click', showFavoritesList)
+    hideButton.addEventListener('click', onHideButtonClick)
     showFavoritesList()
+
+    closeSupportAdsModal()
     setInterval(()=>{
         showFavoritesList()
     },  10000)
